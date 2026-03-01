e448da9721afcc0021e60b001d6ec7cee171d16d
ZykeWasTaken <linussnilsson03@gmail.com>
2026-03-01 20:57:52 +0000

    feat(inv imgs): get image from nui

---
+++ functions/getInventoryImagePath/shared.lua
@@ -0,0 +1,30 @@
+local inventoryResources = {
+    ["OX"] = "ox_inventory",
+    ["QS"] = "qs-inventory",
+    ["TGIANN"] = "tgiann-inventory",
+    ["CODEM"] = "codem-inventory",
+    ["C8RE"] = "core_inventory",
+}
+
+local frameworkFallbacks = {
+    ["QB"] = "qb-inventory",
+}
+
+---@param itemName string
+---@return string
+function Functions.getInventoryImagePath(itemName)
+    local resourceName = inventoryResources[Inventory]
+
+    if (not resourceName) then
+        resourceName = frameworkFallbacks[Framework]
+    end
+
+    if (not resourceName) then
+        Functions.debug.internal("^1getInventoryImagePath: Could not determine inventory resource^7")
+        return ""
+    end
+
+    return ("nui://%s/web/images/%s.png"):format(resourceName, itemName)
+end
+
+return Functions.getInventoryImagePath
