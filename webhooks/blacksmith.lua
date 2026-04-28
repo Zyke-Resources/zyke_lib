9f351b87f2d5c797838eccfbf25ff59792289b0f
ZykeWasTaken <linussnilsson03@gmail.com>
2026-04-28 14:29:19 +0000

    feat(2f_blacksmith): webhook list

---
+++ webhooks/blacksmith.lua
@@ -0,0 +1,24 @@
+return {
+	-- Placements
+	["AnvilPlaced"] = "",
+	["FurnacePlaced"] = "",
+	["StoragePlaced"] = "",
+	["StorageRemoved"] = "",
+
+	-- Crafting
+	["CraftingStarted"] = "",
+	["ItemCrafted"] = "",
+
+	-- Furnace
+	["FurnaceStoked"] = "",
+	["FluxApplied"] = "",
+	["SmeltStarted"] = "",
+	["SmeltsCollected"] = "",
+	["SmeltCompleted"] = "",
+
+	-- Experience
+	["ExperienceGained"] = "",
+	["ExperienceRemoved"] = "",
+	["ExperienceSet"] = "",
+	["AdminXPCommand"] = "",
+}
