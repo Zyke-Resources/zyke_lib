2c45e2034ea80dd115becafd5d65e9bb1bc58884
ZykeWasTaken <linussnilsson03@gmail.com>
2026-05-10 02:55:52 +0000

    feat(getItem): attempt different string formats

--- functions/getItem/shared.lua
+++ functions/getItem/shared.lua
@@ -1,7 +1,25 @@
+-- For different inventories, there's different capitalization needs (primarily for weapons)
+-- First, just check if the item is valid under the normal name
+-- If we can't find it as valid, try uppercase/lowercase
+
 ---@param name string
----@return Item | nil
+---@return Item | nil, string | nil @ Item, normalized name
 function Functions.getItem(name)
-    return Items[name] and Formatting.formatItem(Items[name]) or nil
+    if (Items[name]) then
+        return Formatting.formatItem(Items[name]), name
+    end
+
+    local lowercase = name:lower()
+    if (Items[lowercase]) then
+        return Formatting.formatItem(Items[lowercase]), lowercase
+    end
+
+    local uppercase = name:upper()
+    if (Items[uppercase]) then
+        return Formatting.formatItem(Items[uppercase]), uppercase
+    end
+
+    return nil
 end
 
 return Functions.getItem
