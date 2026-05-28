3b5e4c6822041260ddb72cb39e54adca0aea1191
ZykeWasTaken <linussnilsson03@gmail.com>
2026-05-28 07:12:47 +0000

    feat(numbers): clamp

--- functions/numbers/shared.lua
+++ functions/numbers/shared.lua
@@ -9,4 +9,15 @@ function Functions.numbers.round(number, decimals)
     return math.floor(number * multiplier + 0.5) / multiplier
 end
 
+---@param value number
+---@param min number
+---@param max number
+---@return number
+function Functions.numbers.clamp(value, min, max)
+    if (value < min) then return min end
+    if (value > max) then return max end
+
+    return value
+end
+
 return Functions.numbers
