2a0d1a3b4273b28f169dba02dbe85c4f8df1f5fb
ZykeWasTaken <linussnilsson03@gmail.com>
2026-06-17 22:46:01 +0000

    feat(getItem): guard Items until loaded

--- functions/getItem/shared.lua
+++ functions/getItem/shared.lua
@@ -5,6 +5,10 @@
 ---@param name string
 ---@return Item | nil, string | nil @ Item, normalized name
 function Functions.getItem(name)
+    -- All mentions of the Items table goes through here first
+    -- We guard it, in case the Items list hasn't been resolved yet
+    while (not HasLoaderFinished or not Items) do Wait(10) end
+
     if (Items[name]) then
         return Formatting.formatItem(Items[name]), name
     end
